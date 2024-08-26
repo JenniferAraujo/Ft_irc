e8ced9c848b1d878740e5d940e93cba442a84b14
Diogo <diogo.nogueirasousa123@gmail.com>
2024-08-27 00:13:44 +0100

    TOPIC Parser minor bug fixing

--- src/Commands/Topic.cpp
+++ src/Commands/Topic.cpp
@@ -5,7 +5,7 @@ Topic::Topic(Server& server, Client& client): ACommand("TOPIC", server, client) 
 void Topic::parsing(std::istringstream &input){
 	std::string token;
     int n = 0;
-    while (std::getline(input, token, ' ') || n < 1) {
+    while (std::getline(input, token, ' ') || n < 2) {
         this->trimChar(token, '\r');
         if (token.empty()) {
             this->_error = NEEDMOREPARAMS;          //TOPIC  #a
@@ -30,14 +30,14 @@ void Topic::parsing(std::istringstream &input){
             case 1:
                 if (token[0] == ':') {
                     token.erase(token.begin());
-                    this->_msg = token;			//TOPIC #a :Ajuda
-                }
-                else {
-                    this->_error = NEEDMOREPARAMS;	//TOPIC #a Ajuda
-                    return ;
+                    if (token.empty())
+                        this->_removeTopic = true;  //TOPIC #a :
+                    else
+                        this->_msg = token;			//TOPIC #a :Ajuda
                 }
                 break;
         }
+        n++;
     }
 }
 
@@ -46,5 +46,5 @@ void Topic::execute() {
 }
 
 void Topic::print() const{
-    std::cout << "Command: " << this->_name <<  " | Error: " << this->_error << " | Token: ";
+    std::cout << "Command: " << this->_name <<  " | Error: " << this->_error << " | Channel: " << this->_channel << " | Msg: " << this->_msg << " | Remove Topic: " << this->_removeTopic << std::endl;
 }
